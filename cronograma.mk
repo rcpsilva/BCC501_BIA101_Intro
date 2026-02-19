adab41dcf067ba700bf18d9d57408f03b1494827
rcpsilva <rcpsilva@gmail.com>
2026-02-18 21:29:55 -0300

    Create cronograma.mk

---
+++ cronograma.mk
@@ -0,0 +1,38 @@
+ Semana     & Conteúdo                                          	
+ ---------- &---------------------------------------------------	
+ 1 			& (Feriado) Sexta-Feira Santa
+ 2			& O Departamento de Computação, Bacharelado em Inteligência Artificial e Bacharelado em Ciência da Computação                    					
+ 3 			& Áreas de Atuação, Pesquisa e Extensão
+ 4 			& Prova 1
+ 5 			& (Feriado) Dia do Trabalhador
+ 6			& Estrutura da Universidade e Colegiado do Curso
+ 7			& O Comportamento do Aluno
+ 8 			& Implicações Éticas, Sociais e Ambientais
+ 9 			& Prova 2   
+ 10 		& (Feriado) Corpus Christi 
+ 11         & Seminários
+ 12			& Seminários
+ 13 		& Seminários
+ 14 		& Prova 3
+ 15 		& Seminários
+ 16			& Seminários
+ 17 		& Prova 4
+ 18 		& Exame Especial (31/07/2026)                        			
+
+Metodologia:
+
+- Aulas expositivas sobre o conteúdo programático.
+
+- Seminários com professores e profissionais convidados.
+
+- Provas: Testes individuais feitos durante as aulas.
+
+- Leituras recomendadas: leitura de textos técnicos com a finalidade de proporcionar ao discente a oportunidade de consulta e desenvolvimento de sua capacidade de análise, síntese e crítica de uma bibliografia específica.
+
+Exame Especial: Os alunos que tiverem pelo menos 75% de frequência (mínimo para aprovação) e média inferior a seis pontos poderão fazer o Exame Especial ou o Exame Especial Parcial. Estes exames serão provas únicas, individuais.
+
+Atividades avaliativas:
+
+- 4 provas no valor de 10 pontos.
+
+Nota final = 0.15 * P1 + 0.2 * P2 + 0.2 * P3 + 0.45 * P4
